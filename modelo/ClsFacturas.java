c5a0c955b60589eefa78341b5ee59f7a0bd3d2e9
CorbaYe <corba.ye99@gmail.com>
2023-09-27 07:35:14 -0500

    Espacios

--- modelo/ClsFacturas.java
+++ modelo/ClsFacturas.java
@@ -15,21 +15,17 @@ public class ClsFacturas {
     } 
     //Getters
 
-
     public String getCodigoProducto() {
         return codigoProducto;
     }
-
 
     public int getCantidad() {
         return cantidad;
     }
 
-
     public float getValorVenta() {
         return valorVenta;
     }
-
 
     public float getTotal() {
         return total;
